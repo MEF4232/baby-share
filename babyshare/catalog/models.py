90c23d81ec6d739cfdb23c464df91e1d7a088563
Atharva Dhupkar <atharvadhupkar@atharvas-mbp.wireless.rit.edu>
2020-02-29 21:05:13 -0500

    migrated

--- babyshare/catalog/models.py
+++ babyshare/catalog/models.py
@@ -9,10 +9,14 @@ import uuid
 class User(models.Model):
     """Model representing a user genre."""
     # FIELDS
-    first_name = models.CharField(max_length=20, help_text="First Name", verbose_name="First Name")
-    last_name = models.CharField(max_length=20, help_text="Last Name", verbose_name="Last Name")
-    email = models.EmailField(max_length=100, help_text="Email", verbose_name="Email ID")
-    password = models.CharField(max_length=20, help_text="Password", verbose_name="Password")
+    first_name = models.CharField(max_length=20, help_text="First Name", verbose_name="First Name", null=True)
+    last_name = models.CharField(max_length=20, help_text="Last Name", verbose_name="Last Name", null=True)
+    email = models.EmailField(max_length=100, help_text="Email", verbose_name="Email ID", null=True)
+    password = models.CharField(max_length=20, help_text="Password", verbose_name="Password", null=True)
+    # id = models.IntegerField()
+
+    # models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular user'
+    #                                                                        ' across whole library')
 
     # METHODS
     def __str__(self):
@@ -22,28 +26,30 @@ class User(models.Model):
 
     def get_absolute_url(self):
         """Returns the url to access a particular instance of MyModelName."""
-        return reverse('model-detail-view', args=[str(self.__str__())])
+        # return reverse('model-detail-view', args=[str(self.id)])
+        pass
 
     # CREATE A NEW RECORD
-    record = User(first_name, last_name, email, password)
-    record.save()
+    # record = User(first_name, last_name, email, password)
+    # record.save()
 
 
 class Item(models.Model):
     """Model representing an item."""
     # Fields
-    product_name = models.CharField(max_length=200, help_text='Enter a product name')
-    product_id = models.IntegerField()
-    price = models.FloatField()
+    product_name = models.CharField(max_length=200, help_text='product name', verbose_name="Product Name", null=True)
+    product_id = models.IntegerField(default=0, help_text='product id')
+    price = models.FloatField(default=0.0, help_text='price')
 
-    # Create Record (Item)
-    record = Item(product_name, product_id, price)
-    record.save()
+    # # Create Record (Item)
+    # record = Item(product_name, product_id, price)
+    # record.save()
 
     # Methods
     def get_absolute_url(self):
-        """Returns the url to access a particular instance of MyModelName."""
-        return reverse('model-detail-view', args=[str(self.product_id)])
+        pass
+    #     """Returns the url to access a particular instance of MyModelName."""
+    #     return reverse('model-detail-view', args=[str(self.product_id)])
 
     def __str__(self):
         """String for representing the Model object."""
