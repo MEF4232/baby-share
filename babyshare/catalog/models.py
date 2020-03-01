7d60d230eb3c5c42c204dae868ae59873617ce7f
sea2985 <sea2985@g.rit.edu>
2020-02-29 22:10:42 -0500

    Stubbed out update-item-model (created constructor)

--- babyshare/catalog/models.py
+++ babyshare/catalog/models.py
@@ -33,26 +33,11 @@ class Item(models.Model):
         return self.product_name
 
 
-class Category(models.Model):
-    """Model representing the category."""
-    name = models.CharField(max_length=200, help_text='Enter the item category.')
+class Inventory(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
     def __str__(self):
         """String for representing the Model object."""
         return self.name
 
-    # TODO Add to Item:
-    # category = models.ManyToManyField(Category, help_text='Select a category for this item')
-
-class Subcategory(models.Model):
-    """Model representing the subcategory."""
-    name = models.CharField(max_length=200, help_text='Enter the item subcategory.')
-    
-    category = models.ManyToManyField(Category, help_text='Select a category for this item')
-   
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.name
-
-    # TODO Add to Item:
-    # subcategory = models.ManyToManyField(Subcategory, help_text='Select a subcategory for this item')
