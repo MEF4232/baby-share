f9d80b50efcba5a927de9c186a58841cfb8f1da1
sea2985 <60417016+sea2985@users.noreply.github.com>
2020-02-29 20:03:49 -0500

    Update item model (#5)
    * Stubbed out update-item-model (created constructor)
    
    * Stubbed out update-item-model (created constructor)

--- babyshare/catalog/models.py
+++ babyshare/catalog/models.py
@@ -13,12 +13,25 @@ class User(models.Model):
         return self.name
 
 class Item(models.Model):
-    """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    """Model representing an item."""
+    # Fields
+    product_name = models.CharField(max_length=200, help_text='Enter a product name')
+    product_id = models.IntegerField()
+    price = models.FloatField()
+
+    # Create Record (Item)
+    record = Item(product_name, product_id, price)
+    record.save()
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.product_id)])
 
     def __str__(self):
         """String for representing the Model object."""
-        return self.name
+        return self.product_name
+
 
 class Category(models.Model):
     """Model representing the category."""
